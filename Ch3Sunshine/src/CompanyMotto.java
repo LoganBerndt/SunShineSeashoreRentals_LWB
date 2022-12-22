bf9d25f14278787cc2f8846b6ed304a213ec539e
LoganBerndt <116930335+LoganBerndt@users.noreply.github.com>
2022-12-21 18:40:18 -0600

    Added First Code
    Added Code for the first part of the seashore rentals.

---
+++ Ch3Sunshine/src/CompanyMotto.java
@@ -0,0 +1,15 @@
+public class CompanyMotto {
+
+    public static void main(String[] args) {
+        output();
+    }
+
+    public static void output(){
+
+        System.out.println(("******************************"));
+        System.out.println(("** SunShine Seashore Galore **"));
+        System.out.println(("******************************"));
+
+    }
+
+}
